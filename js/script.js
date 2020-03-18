d67c4c7f9aa5901bdc10bdc0215fdf3a8b3af9c2
Никита <killerlord1988@gmail.com>
2020-03-18 23:26:32 +0400

    доделали полностью проект в соотвествии стайлгайдом и работующим js

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-    storage = localStorage.getItem("name");
-  } catch (err) {
-    isStorageSupport = false;
-  }
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -30,32 +30,32 @@ link.addEventListener("click", function (evt) {
 });
 
 close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-show");
     popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
+    }
+  }
 });
 
-form.addEventListener("submit", function(evt) {
-    if (!name.value || !email.value) {
-evt.preventDefault();
-popup.classList.remove("modal-error");
-popup.offsetWidth = popup.offsetWidth;
-popup.classList.add("modal-error");
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("name", name.value);
-          }
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
-});
-
-window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-            popup.classList.remove("modal-error");
-        } 
-    }
+  }
 });
 
 /*modal-map*/
@@ -65,24 +65,129 @@ var mapPopup = document.querySelector(".modal-map");
 
 var close = mapPopup.querySelector(".modal-content-close");
 
-mapLink.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    mapPopup.classList.add("modal-show");
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
 });
 
 close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
     evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (mapPopup.classList.contains("modal-show")) {
-            mapPopup.classList.remove("modal-show");
-        } 
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
     }
+  }
 });
 
-/*slider*/
+// /*slider*/
+
+// var slideIndex = 0;
+// var dotIndex = 0;
+
+// var i;
+// var slides = document.querySelectorAll(".slider-wrapper");
+// var dots = document.querySelectorAll(".slider-radio");
+
+// showSlides();
+// changeSlides();
+
+// function showSlides() {
+
+
+//   console.log(dots);
+
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   };
+
+//   slideIndex++;
+
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   };
+
+
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].removeAttribute("checked", "checked");
+//     dots[dotIndex].setAttribute("checked", "checked");
+//     console.log(dots[i]);
+//   };
+
+//   dotIndex++;
+
+//   console.log(dotIndex);
+
+//   if (dotIndex > dots.length - 1) {
+//     dotIndex = 0;
+//   };
+
+
+//   slides[slideIndex - 1].style.display = "flex";
+
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+//   debugger
+// };
+
+// function changeSlides() {
+//   for (i = 0; i < dots.length; i++) {
+//     if (dots[i].type == "radio" && dots[i].checked) {
+//       console.log(dots[i])
+//       slides[i].style.display = "flex";
+//     }
+//   };
+// };
+
+
+
+const intervalId = setInterval(changeSlide, 2000);
+
+const slides = document.querySelectorAll(".slider-wrapper");
+const dots = document.querySelectorAll(".slider-control");
+
+let currentIndex = 0;
+
+function changeSlide () {
+  if (currentIndex === slides.length ) {
+    currentIndex = 0;
+  }
+
+  slides.forEach((slide, idx) => {
+    if (idx === currentIndex) {
+      slide.style.display = 'flex';
+      dots[idx].classList.add('active');
+    } else {
+ 
+      slide.style.display = 'none';
+      dots[idx].classList.remove('active');  
+    }
+  });
+
+  currentIndex++;
+};
+
+function removeStyle(el) {
+  el.style.display = 'none';
+};
+
+function removeClass(el) {
+  el.classList.remove('active');
+}
+
+dots.forEach(dot => {
+  dot.addEventListener('click', evt => {
+    const id = evt.target.dataset.id;
+
+    slides.forEach(removeStyle);
+    slides[id].style.display = 'flex';
+    
+    dots.forEach(removeClass);
+    dots[id].classList.add('active');
+
+  });
+})
 
